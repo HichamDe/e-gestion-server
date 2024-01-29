6c62d82efb1bf24d1a3f2fe1f82ab33fcca97cc3
hichamDe <hicham.iamiri200@gmail.com>
2024-01-29 10:56:15 +0100

    adding apis

--- routes/api.php
+++ routes/api.php
@@ -20,16 +20,16 @@ use App\Http\Controllers\AuthenticationController;
 |
 */
 
-Route::get("/products",function(){
+Route::get("/products", function () {
     return response()->json(Produit::all());
 });
+Route::post('login', [AuthenticationController::class, 'login']);
+Route::post('register', [AuthenticationController::class, 'register']);
 
-// Route::post('login', [AuthenticationController::class, 'login']);
-// Route::post('register', [AuthenticationController::class, 'register']);
 // Route::middleware('auth:sanctum')->group(function () {
 //     Route::resource('clients', ClientController::class);
 // });
- 
+
 // Route::get("/commande/{id}",function(int $id){
 //     return new CommandeResource(Commande::findOrFail($id));
 // })->middleware("auth:sanctum");
